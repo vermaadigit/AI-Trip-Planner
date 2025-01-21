31d8c0c8df9ca87cac58ded9eaa94873ef20af31
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-01-21 10:30:21 +0530

    main.jsx modified

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router'
+import CreateTrip from './create-trip/index.jsx'
+import Header from './components/custom/Header'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  },
+  {
+    path: '/create-trip',
+    element: <CreateTrip/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+    <RouterProvider router={router} / >
   </StrictMode>,
 )
