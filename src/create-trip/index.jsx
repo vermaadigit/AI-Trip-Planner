db0dded857687e1923a0868d469fa0bd0f9f1b38
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-01-22 13:32:00 +0530

    Create-trip Index File

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 import { Input } from '../components/ui/input'
-import { SelectBudgetOptions } from '../constants/options'
+import { AI_PROMPT, SelectBudgetOptions } from '../constants/options'
 import { SelectTravelesList } from '../constants/options'
 import { Button } from '../components/ui/button'
 import { toast } from "sonner"
+import { chatSession } from '@/service/AIModal'
 
 
 function  CreateTrip () {
@@ -24,12 +25,24 @@ function  CreateTrip () {
     console.log(formData);
   },[formData])
 
-  const OnGenerateTrip = () => {
+  const OnGenerateTrip = async() => {
     if(formData?.noOfDays>5 && !formData?.location || !formData?.budget || !formData?.people){
       toast("Please fill all Details")
       return ;
     }
-    console.log(formData);
+    
+    const FINAL_PROMPT = AI_PROMPT
+    .replace('{location}', formData?.location?.label)
+    .replace('{totalDays}', formData?.noOfDays)
+    .replace('{people}', formData?.people)
+    .replace('{budget}', formData?.budget)
+    .replace('{totalDays}', formData?.noOfDays)
+
+    console.log(FINAL_PROMPT);
+
+    const result = await chatSession.sendMessage(FINAL_PROMPT);
+
+    console.log(result?.response?.text());
   }
 
   return (
