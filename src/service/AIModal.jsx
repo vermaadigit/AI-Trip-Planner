7ee0608e6e88ba604701331a5888b1b3918ca1be
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-01-22 13:41:43 +0530

    Gemini-3.0 Exp Model

--- src/service/AIModal.jsx
+++ src/service/AIModal.jsx
@@ -1,8 +1,4 @@
-const {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } = require("@google/generative-ai");
+import { GoogleGenerativeAI } from "@google/generative-ai";
   
   const apiKey = import.meta.env.VITE_GOOGLE_GEMINI_AI_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
