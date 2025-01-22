f9d9b435aafdc794831d3802e1e0df7225369141
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-01-22 15:48:46 +0530

    Import Google OAuth Service

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { RouterProvider } from 'react-router'
 import CreateTrip from './create-trip/index.jsx'
 import Header from './components/custom/Header'
 import { Toaster } from './components/ui/sonner'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const router = createBrowserRouter([
   {
@@ -21,8 +22,10 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
     <Header />
     <Toaster />
     <RouterProvider router={router} / >
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
