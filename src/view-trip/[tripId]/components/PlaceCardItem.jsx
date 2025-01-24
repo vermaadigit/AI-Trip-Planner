2a49d9792a307e3153caece47d144d83bf19fd2c
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-01-25 04:53:37 +0530

    Photo URL Reference

--- src/view-trip/[tripId]/components/PlaceCardItem.jsx
+++ src/view-trip/[tripId]/components/PlaceCardItem.jsx
@@ -2,12 +2,33 @@ import { Button } from '@/components/ui/button'
 import React from 'react'
 import { FaMapLocationDot } from "react-icons/fa6";
 import { Link } from 'react-router-dom'
+import { GetPlaceDetails } from '@/service/GlobalApi';
+import { useState } from 'react';
+import { PHOTO_REF_URL } from '@/service/GlobalApi';
+import { useEffect } from 'react';  
 
 function PlaceCardItem({place}) {
+
+  const [photoUrl, setPhotoUrl]= useState();
+    useEffect(()=>{
+      place && GetPlacePhoto()
+    },[place])
+  
+    const GetPlacePhoto = () => {
+      const data={
+        textQuery : place.placeName
+      }
+      const result = GetPlaceDetails(data).then(resp=>{
+        console.log(resp.data.places[0].photos[3].name);
+  
+        const PhotoURL = PHOTO_REF_URL.replace('{NAME}',resp.data.places[0].photos[3].name);
+        setPhotoUrl(PhotoURL);
+      })
+    }
   return (
     <Link to={'https://www.google.com/maps/search/?api=1&query='+place.placeName} target='_blank'>
     <div className='border rounded-xl p-3 mt-2 flex gap-5 hover:scale-105 transition-all hover:shadow-md cursor-pointer'>
-        <img src="/Placeholder.jpg" className='w-[130px] h-[130px] rounded-xl' />
+        <img src={photoUrl?photoUrl:'/Placeholder.jpg'} className='w-[130px] h-[130px] rounded-xl object-cover' />
         <div>
             <h2 className='font-bold text-lg'>{place.placeName}</h2>
             <p className='text-sm text-gray-400'>{place.placeDetails}</p>
