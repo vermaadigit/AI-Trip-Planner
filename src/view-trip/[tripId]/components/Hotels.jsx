0c4220cfca4085202cf0689784d0faaae8de1a63
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-01-25 04:52:29 +0530

    Hotel Data

--- src/view-trip/[tripId]/components/Hotels.jsx
+++ src/view-trip/[tripId]/components/Hotels.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import HotelCardItem from './HotelCardItem'
 
 function Hotels({trip}) {
   return (
@@ -8,17 +9,7 @@ function Hotels({trip}) {
 
         <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5'>
             {trip?.tripData?.hotels?.map((hotel,index)=>(
-                <Link to={'https://www.google.com/maps/search/?api=1&query='+hotel.hotelName+","+hotel?.hotelAddress} target='_blank'>
-                <div className='hover:scale-105 transition-all cursor-pointer'>
-                    <img src="/Placeholder.jpg" className='rounded-lg'/>
-                    <div className='my-3 flex flex-col gap-2'>
-                        <h2 className='font-medium'>{hotel?.hotelName}</h2>
-                        <h2 className='text-xs text-gray-500'>📍 {hotel?.hotelAddress}</h2>
-                        <h2 className='text-sm '>💰 {hotel?.price}</h2>
-                        <h2 className='text-sm'>⭐ {hotel.rating}</h2>
-                    </div>
-                </div>
-                </Link>
+                <HotelCardItem hotel={hotel}/>
             ))}
         </div>
     </div>
