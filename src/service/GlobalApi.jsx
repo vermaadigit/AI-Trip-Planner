0c9d5b08d7540d977823c49ddd3a400a487ebb09
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-01-25 04:51:25 +0530

    Google Place API

--- src/service/GlobalApi.jsx
+++ src/service/GlobalApi.jsx
@@ -14,4 +14,6 @@ const config = {
     }
 }
 
-export const GetPlaceDetails=(data)=>axios.post(BASE_URL,data,config.headers)
+export const GetPlaceDetails=(data)=>axios.post(BASE_URL,data,config)
+
+export const PHOTO_REF_URL = 'https://places.googleapis.com/v1/{NAME}/media?maxHeightPx=1000&maxWidthPx=1000&key='+import.meta.env.VITE_GOOGLE_PLACE_API_KEY;
